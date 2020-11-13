b4462620255e8ecc0c0f5ab0284c252dc21963f3
Xpost2000 <xpost2000cod@gmail.com>
2020-11-13 17:49:18 -0500

    utiltiy split

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ TODO: Please rewrite the tokenizer.
 */
 mod markup;
 use self::markup::*;
-
+mod utility;
+use self::utility::*;
 /*
 I kind of want the slideshow to be a state machine API.
 Kind of in the same way OpenGL is I guess.
@@ -381,46 +382,11 @@ fn compile_slide(slide_source : &String) -> Slide {
     slide
 }
 
-fn remove_comments_from_source(source : &str) -> String {
-    let mut filtered = String::new();
-    let lines : Vec<&str> = source.split("\n").collect();
-    for line in lines.iter() {
-        if !(line.chars().nth(0) == Some('#')) {
-            for character in line.chars() {
-                filtered.push(character);
-            }
-            filtered.push('\n');
-        }
-    }
-    filtered
-}
-
-fn load_file(file_name: &str) -> String {
-    use std::io::Read;
-    use std::fs::File;
-
-    let mut result = String::new();
-    let mut slide_file = File::open(file_name)
-        .expect("There was an error in reading the file!");
-    slide_file.read_to_string(&mut result).expect("Unable to read into string");
-    result
-}
-
 extern crate sdl2;
 
 use sdl2::pixels::Color as SDLColor;
 use sdl2::event::Event as SDLEvent;
 use sdl2::keyboard::Keycode as SDLKeycode;
-
-fn clamp_i32(x: i32, min: i32, max: i32) -> i32 {
-    if x < min {
-        min
-    } else if x > max {
-        max
-    } else {
-        x
-    }
-}
 
 fn main() {
     if false {
---
+++ src/utility.rs
@@ -0,0 +1,34 @@
+pub fn clamp_i32(x: i32, min: i32, max: i32) -> i32 {
+    if x < min {
+        min
+    } else if x > max {
+        max
+    } else {
+        x
+    }
+}
+
+pub fn remove_comments_from_source(source : &str) -> String {
+    let mut filtered = String::new();
+    let lines : Vec<&str> = source.split("\n").collect();
+    for line in lines.iter() {
+        if !(line.chars().nth(0) == Some('#')) {
+            for character in line.chars() {
+                filtered.push(character);
+            }
+            filtered.push('\n');
+        }
+    }
+    filtered
+}
+
+pub fn load_file(file_name: &str) -> String {
+    use std::io::Read;
+    use std::fs::File;
+
+    let mut result = String::new();
+    let mut slide_file = File::open(file_name)
+        .expect("There was an error in reading the file!");
+    slide_file.read_to_string(&mut result).expect("Unable to read into string");
+    result
+}
