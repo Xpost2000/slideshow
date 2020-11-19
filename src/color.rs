d11acf42b533ab2ec6e8e285f2c2a2217d7078d4
Xpost2000 <xpost2000cod@gmail.com>
2020-11-18 22:09:26 -0500

    split into more modules

---
+++ src/color.rs
@@ -0,0 +1,63 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+pub fn hexadecimal_to_decimal(literal: &str) -> u8 {
+    let mut result : u8 = 0;
+    for (index, ch) in literal.chars().enumerate() {
+        let index = (literal.len()-1) - index;
+        result +=
+            match ch {
+                '0' => {0}, '1' => {1}, '2' => {2},
+                '3' => {3}, '4' => {4}, '5' => {5},
+                '6' => {6}, '7' => {7}, '8' => {8},
+                '9' => {9}, 'A' => {10}, 'B' => {11},
+                'C' => {12}, 'D' => {13}, 'E' => {14}, 'F' => {15},
+                _ => { panic!("undefined character for hexadecimal literal!"); }
+            } * 16_u8.pow(index as u32);
+    }
+    result
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Color {
+        Color {r, g, b, a}
+    }
+
+    pub fn parse_hexadecimal_literal(hex: &str) -> Option<Color> {
+        // TODO : This is not perfectly safe since it'll be fine up to
+        // 255... This needs to be templated.
+
+        if let Some('#') = hex.chars().nth(0) {
+            let hex = &hex[1..];
+            match hex.len() {
+                // FFFFFF
+                // FFFFFFFF
+                6 | 8 => {
+                    Some(Color::new(
+                        hexadecimal_to_decimal(&hex[0..2]),
+                        hexadecimal_to_decimal(&hex[2..4]),
+                        hexadecimal_to_decimal(&hex[4..6]),
+                        if hex.len() == 8 {
+                            hexadecimal_to_decimal(&hex[6..8])
+                        } else {
+                            255
+                        }
+                    ))
+                },
+                _ => {
+                    println!("Error parsing hexadecimal literal");
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+pub const COLOR_WHITE : Color = Color {r: 255, g: 255, b: 255, a: 255};
+pub const COLOR_BLACK : Color = Color {r: 0, g: 0, b: 0, a: 0};
---
+++ src/graphics_context.rs
@@ -0,0 +1,175 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color as SDLColor;
+use std::collections::HashMap;
+
+use crate::Color;
+type SDL2WindowCanvas = sdl2::render::Canvas<sdl2::video::Window>;
+// hashmaps of hashmaps?
+// Yeah this is not a good idea, but whatever for now.
+
+// TODO: convert this to &str.
+struct SDL2FontAsset<'ttf> {
+    file_name: String,
+    stored_sizes : HashMap<u16, sdl2::ttf::Font<'ttf, 'static>>,
+}
+
+impl<'ttf> SDL2FontAsset<'ttf> {
+    fn new(file_name: String) -> SDL2FontAsset<'ttf> {
+        SDL2FontAsset {file_name: file_name.clone(), stored_sizes: HashMap::new(),}
+    }
+
+    fn new_with_common_sizes(file_name: String, ttf_context: &'ttf sdl2::ttf::Sdl2TtfContext) -> SDL2FontAsset<'ttf> {
+        SDL2FontAsset {
+            file_name: file_name.clone(),
+            stored_sizes: {
+                let mut okay_table_i_guess = HashMap::new();
+                // probably common sizes.
+                for font_size in &[10, 11, 12, 14, 16, 18, 24, 32, 36, 48, 64, 72, 84, 96, 128] {
+                    okay_table_i_guess.insert(*font_size, ttf_context.load_font(file_name.clone(), *font_size).unwrap());
+                }
+
+                okay_table_i_guess
+            }
+        }
+    }
+
+    fn load_size_if_not_loaded(&mut self, font_size: u16, ttf_context: &'ttf sdl2::ttf::Sdl2TtfContext) {
+        if let None = self.stored_sizes.get(&font_size) {
+            self.stored_sizes.insert(font_size, ttf_context.load_font(self.file_name.clone(), font_size).unwrap());
+        }
+    }
+
+    fn get_size(&mut self, font_size: u16, ttf_context: &'ttf sdl2::ttf::Sdl2TtfContext) -> &sdl2::ttf::Font<'ttf, 'static> {
+        self.load_size_if_not_loaded(font_size, ttf_context);
+        self.stored_sizes.get(&font_size).unwrap()
+    }
+
+    fn get_size_mut(&mut self, font_size: u16, ttf_context: &'ttf sdl2::ttf::Sdl2TtfContext) -> &mut sdl2::ttf::Font<'ttf, 'static> {
+        self.load_size_if_not_loaded(font_size, ttf_context);
+        self.stored_sizes.get_mut(&font_size).unwrap()
+    }
+}
+
+pub struct SDL2GraphicsContext<'ttf> {
+    window_canvas : SDL2WindowCanvas,
+    ttf_context : &'ttf sdl2::ttf::Sdl2TtfContext,
+    font_assets : HashMap<String, SDL2FontAsset<'ttf>>,
+}
+
+// lots of interface and safety changes to be made.
+impl<'ttf> SDL2GraphicsContext<'ttf> {
+    pub fn new(window: sdl2::video::Window, ttf_context : &'ttf sdl2::ttf::Sdl2TtfContext) -> SDL2GraphicsContext<'ttf> {
+        SDL2GraphicsContext {
+            window_canvas: window.into_canvas().build().unwrap(),
+            ttf_context,
+            font_assets: HashMap::new()
+        }
+    }
+
+    pub fn add_font<'a>(&mut self, font_name: &'a str) -> &'a str {
+        self.font_assets.insert(font_name.to_owned(),
+                                SDL2FontAsset::new_with_common_sizes(font_name.to_owned(), &self.ttf_context));
+        font_name
+    }
+
+    pub fn window(&self) -> &sdl2::video::Window {
+        self.window_canvas.window()
+    }
+
+    pub fn window_mut(&mut self) -> &mut sdl2::video::Window {
+        self.window_canvas.window_mut()
+    }
+
+    pub fn present(&mut self) {
+        self.window_canvas.present();
+    }
+
+    pub fn clear_color(&mut self, clear_color: Color) {
+        self.window_canvas.set_draw_color(
+            SDLColor::RGBA(
+                clear_color.r,
+                clear_color.g,
+                clear_color.b,
+                clear_color.a,
+            )
+        );
+        self.window_canvas.clear();
+    }
+
+    pub fn get_font_asset(&self, font_id: &str) -> Option<&SDL2FontAsset<'ttf>> {
+        self.font_assets.get(font_id)
+    } 
+
+    pub fn get_font_asset_mut(&mut self, font_id: &str) -> Option<&mut SDL2FontAsset<'ttf>> {
+        self.font_assets.get_mut(font_id)
+    } 
+
+    pub fn find_text_asset_by_size(&mut self, font_id: &str, font_size: u16) -> Option<&sdl2::ttf::Font<'ttf, 'static>> {
+        let ttf_context = self.ttf_context;
+        let font_asset = self.get_font_asset_mut(font_id);
+
+        if let Some(font_asset) = font_asset {
+            Some(font_asset.get_size(font_size, ttf_context))
+        } else {
+            None
+        }
+    }
+
+    pub fn find_text_asset_by_size_mut(&mut self, font_id: &str, font_size: u16) -> Option<&mut sdl2::ttf::Font<'ttf, 'static>> {
+        let ttf_context = self.ttf_context;
+        let font_asset = self.get_font_asset_mut(font_id);
+
+        if let Some(font_asset) = font_asset {
+            Some(font_asset.get_size_mut(font_size, ttf_context))
+        } else {
+            None
+        }
+    }
+
+    pub fn text_dimensions(&mut self, font_id: &str, text: &str, font_size: u16) -> (u32, u32) {
+        if let Some(font_at_size) = self.find_text_asset_by_size(font_id, font_size) {
+            let (width, height) = font_at_size.size_of(text).unwrap();
+            (width, height)
+        } else {
+            (0, 0)
+        }
+    }
+
+    pub fn render_text(&mut self, font_id: &str, x: f32, y: f32, text: &str, font_size: u16, color: Color, style: sdl2::ttf::FontStyle) {
+        let ttf_context = self.ttf_context;
+        let texture_creator = self.window_canvas.texture_creator();
+
+        match self.find_text_asset_by_size_mut(font_id, font_size) {
+            Some(font) => {
+                if font.get_style() != style {
+                    font.set_style(style);
+                }
+                let mut texture = texture_creator.create_texture_from_surface(
+                    &font.render(text)
+                        .blended(SDLColor::RGBA(255, 255, 255, 255))
+                        .expect("how did this go wrong?")
+                ).expect("how did this go wrong?");
+
+                texture.set_color_mod(color.r, color.g, color.b);
+                texture.set_alpha_mod(color.a);
+
+                let sdl2::render::TextureQuery { width, height, .. } = texture.query();
+                self.window_canvas.copy(&texture, None, Some(sdl2::rect::Rect::new(x as i32, y as i32, width, height))).unwrap();
+            },
+            None => {}
+        }
+    }
+
+    pub fn render_filled_rectangle(&mut self, x: f32, y: f32, w: f32, h: f32, color: Color) {
+        self.window_canvas.set_draw_color(
+            SDLColor::RGBA(
+                color.r,
+                color.g,
+                color.b,
+                color.a
+            )
+        );
+        self.window_canvas.fill_rect(sdl2::rect::Rect::new(x as i32, y as i32, w as u32, h as u32));
+    }
+}
